d8205509f42930330ab19be328d62588244cdb46
brad <bholcombe@trivenidigital.com>
2017-06-11 00:42:36 -0400

    Initial Commit

---
+++ CoyoteHotSpots/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CoyoteHotSpots
+//
+//  Created by Bradford Holcombe on 6/11/17.
+//  Copyright © 2017 Bradford Holcombe. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
